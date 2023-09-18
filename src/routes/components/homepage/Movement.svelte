77fb0c688371ded2bfd29517932d669a87b590ca
Nick Bechtel <bechtel.nick@gmail.com>
2023-09-18 11:57:57 -0400

    updates to movement component ui

--- src/routes/components/homepage/Movement.svelte
+++ src/routes/components/homepage/Movement.svelte
@@ -1,7 +1,7 @@
-<div class="w-screen h-[1000px] flex items-center justify-center">
-    <div class="flex flex-col items-center justify-center">
-        <div class="text-secondary-title">Join the movement for content authenticity</div>
-        <div class="text-sub-text text-sub-heading">If we want to make the internet a more safe and useful place, we need new ways to bolster trust in digital content. The Content Authenticity Initiative is a cross-industry organization behind Content Credentials. It does advocacy, education, thought leadership, and develops open-source tools to support the adoption of Content Credentials.</div>
+<div class="w-screen h-[720px] flex items-center justify-center">
+    <div class="h-[60%] w-[60%] flex flex-col items-center justify-evenly">
+        <div class="text-secondary-title text-center">Join the movement for content authenticity</div>
+        <div class="text-sub-text text-sub-heading text-center">If we want to make the internet a more safe and useful place, we need new ways to bolster trust in digital content. The Content Authenticity Initiative is a cross-industry organization behind Content Credentials. It does advocacy, education, thought leadership, and develops open-source tools to support the adoption of Content Credentials.</div>
         <div>
             <button class="bg-brand-red rounded-full px-3 py-2">
                 Learn more
