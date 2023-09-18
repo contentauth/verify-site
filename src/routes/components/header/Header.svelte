2a1187e421f2fdb1e769010cff395f44aa2860c3
Nick Bechtel <bechtel.nick@gmail.com>
2023-09-18 12:57:39 -0400

    added functionality to header elements

--- src/routes/components/header/Header.svelte
+++ src/routes/components/header/Header.svelte
@@ -1,14 +1,25 @@
 <script>
-    import Logo from '../../../../assets/png/cc.png'
+  import Logo from '../../../../assets/png/cc.png';
 </script>
 
 <div class="h-[100px] w-screen">
-  <div class="flex flex-row items-center justify-between pt-6 px-6">
+  <div class="flex flex-row items-center justify-between px-6 pt-6">
     <div class="pr-6">
-        <img src={Logo} width="250px" height="100px" alt="content credentials"/>
+      <a href="/">
+        <img
+          src={Logo}
+          width="250px"
+          height="100px"
+          alt="content credentials" />
+      </a>
     </div>
     <div class="pl-6">
-      <div class="bg-brand-red w-[100px] rounded-full text-center text-general font-bold px-2 py-3">Verify</div>
+      <a href="/">
+        <div
+          class="w-[100px] rounded-full bg-brand-red px-2 py-3 text-center text-general font-bold">
+          Verify
+        </div>
+      </a>
     </div>
   </div>
 </div>
