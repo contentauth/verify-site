3d1f7ec183d32873f07fac2ebd08e4312e54364b
Massy Mahamli <mmahamli@adobe.com>
2023-09-19 15:10:26 -0400

    added focus on top of page & removed space

--- src/routes/verify/components/Compare/ComparePanel/ComparePanel.svelte
+++ src/routes/verify/components/Compare/ComparePanel/ComparePanel.svelte
@@ -37,7 +37,11 @@
 </script>
 
 <div class="z-1">
-  <button class="me-2" on:click={() => verifyStore.setHierarchyView()}  aria-roledescription={$_('sidebar.verify.compare.back')}>
+  <button
+    class="me-2"
+    on:click={() => verifyStore.setHierarchyView()}
+    id="compareButtonID"
+    aria-roledescription={$_('sidebar.verify.compare.back')}>
     <div class="flex px-5 py-5 pb-2">
       <BackArrow class="me-2" />
       <Header>{$_('sidebar.verify.compare')}</Header>
