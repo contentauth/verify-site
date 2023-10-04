bce5949432ae1058ad1afc6413799052f37ed954
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2023-10-04 12:17:58 -0400

    added inspect to header

--- src/components/Home/header/Header.svelte
+++ src/components/Home/header/Header.svelte
@@ -3,30 +3,28 @@
   import MobileLogo from '$assets/svg/logos/homepage/crvertical.svg';
 </script>
 
-<div class="h-[6.25rem] w-screen px-[1.25rem] sm:px-[2rem] xl:px-[2rem]">
-  <div class="flex flex-row items-center justify-between pt-6">
-    <div class="pr-6">
-      <a href="/">
-        <img
-          src={Logo}
-          width="250px"
-          height="100px"
-          alt="content credentials"
-          class="hidden sm:flex" />
-        <img
-          src={MobileLogo}
-          height="46px"
-          alt="content credentials"
-          class="sm:hidden max-h-[46px]" />
-      </a>
+<div class="w-full flex justify-center">
+  <div
+    class="h-[6.25rem] w-full px-[1.25rem] sm:px-[2rem] xl:px-[2rem] max-w-[1440px]">
+    <div class="flex flex-row items-center justify-between pt-6">
+      <div class="pr-6">
+        <a href="/">
+          <img
+            src={Logo}
+            width="250px"
+            height="100px"
+            alt="content credentials"
+            class="hidden sm:flex" />
+          <img
+            src={MobileLogo}
+            height="46px"
+            alt="content credentials"
+            class="sm:hidden max-h-[46px]" />
+        </a>
+      </div>
+      <div>
+        <a href="/verify" class="underline"> Inspect </a>
+      </div>
     </div>
-    <!-- <div class="pl-6">
-      <a href="/verify">
-        <div
-          class="w-[100px] h-[2.75rem] rounded-full bg-brand-red px-[1.25rem] flex items-center justify-center text-center text-general hover:bg-brand-red/80">
-          Verify
-        </div>
-      </a>
-    </div> -->
   </div>
 </div>
