285fbfd112d25bb03ea23ca18cf15caae978a330
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2023-10-04 17:19:04 -0400

    updates to trust component mobile layout

--- src/components/Home/homepage/Trust.svelte
+++ src/components/Home/homepage/Trust.svelte
@@ -67,7 +67,7 @@
 
     <!-- Desktop Triangle Layout 768px - 1280px -->
     <ul
-      class="sm:hidden 2xl:hidden w-full max-w-[90rem] grid-cols-12 gap-[1.25rem] md:block">
+      class="hidden 2xl:hidden w-full max-w-[90rem] grid-cols-12 gap-[1.25rem] md:block">
       <div class="flex flex-col items-center">
         <div class="grid grid-cols-12 gap-[1.25rem]">
           <li class="col-start-1 col-end-7 w-full rounded-[20px]">
@@ -130,15 +130,16 @@
     <!-- Desktop Triangle Layout -->
 
     <!-- Mobile 768px & under -->
-    <ul class="w-full max-w-[90rem] grid-cols-12 gap-[1.25rem] md:hidden">
-      <li class="col-start-1 col-end-5 h-[27.6875rem] w-full rounded-[20px]">
+    <ul
+      class="flex flex-col w-full max-w-[90rem] grid-cols-12 md:hidden gap-[1.25rem]">
+      <li class="col-start-1 col-end-5 w-full rounded-[20px]">
         <div class="h-full">
           <h3
             class="w-full rounded-t-xl bg-brand-orange px-[32px] py-[20px] text-trust-label lg:rounded-t-[20px]">
             Open-source standards
           </h3>
           <div
-            class="2xl:text-trust-desc-2xl h-[365px] rounded-b-xl bg-[#f5f5f5] px-[32px] py-6 text-trust-desc lg:rounded-b-[20px] xl:text-trust-desc-xl">
+            class="2xl:text-trust-desc-2xl h-[208px] rounded-b-xl bg-[#f5f5f5] px-[32px] py-6 text-trust-desc lg:rounded-b-[20px] xl:text-trust-desc-xl">
             <p class="">
               Content Credentials are based on open technical standards
               developed and maintained by the <a
@@ -150,14 +151,14 @@
           </div>
         </div>
       </li>
-      <li class="col-start-5 col-end-9 h-[27.6875rem] w-full rounded-[20px]">
+      <li class="col-start-5 col-end-9 w-full rounded-[20px]">
         <div class="h-full">
           <h3
             class="w-full rounded-t-xl bg-brand-red px-[32px] py-[20px] text-trust-label lg:rounded-t-[20px]">
             Clear tampering detection
           </h3>
           <div
-            class="2xl:text-trust-desc-2xl h-[365px] rounded-b-xl bg-[#f5f5f5] px-[32px] py-6 text-trust-desc lg:rounded-b-[20px] xl:text-trust-desc-xl">
+            class="2xl:text-trust-desc-2xl h-[208px] rounded-b-xl bg-[#f5f5f5] px-[32px] py-6 text-trust-desc lg:rounded-b-[20px] xl:text-trust-desc-xl">
             <p class="">
               Content Credentials use technology that allows any unsupported
               changes to content’s edit history or the content itself to be
@@ -166,14 +167,14 @@
           </div>
         </div>
       </li>
-      <li class="col-start-9 col-end-13 h-[27.6875rem] w-full rounded-[20px]">
+      <li class="col-start-9 col-end-13 w-full rounded-[20px]">
         <div class="h-full">
           <h3
             class="w-full rounded-t-xl bg-brand-violet px-[32px] py-[20px] text-trust-label lg:rounded-t-[20px]">
             Accountability
           </h3>
           <div
-            class="2xl:text-trust-desc-2xl h-[365px] rounded-b-xl bg-[#f5f5f5] px-[32px] py-6 text-trust-desc lg:rounded-b-[20px] xl:text-trust-desc-xl">
+            class="2xl:text-trust-desc-2xl h-[208px] rounded-b-xl bg-[#f5f5f5] px-[32px] py-6 text-trust-desc lg:rounded-b-[20px] xl:text-trust-desc-xl">
             <p class="">
               Organizations that issue Content Credentials must identify
               themselves in the Content Credentials they issue. Issuers
