69e88c7fab127e6d8ee67af4d65d272aa05c04a8
Parijat Saxena <parijatsaxena30@gmail.com>
2023-10-19 12:42:52 -0400

    finishing up misconfigured breakpoints

--- src/components/Home/homepage/Movement.svelte
+++ src/components/Home/homepage/Movement.svelte
@@ -1,12 +1,12 @@
-<div class="flex w-screen items-center justify-center py-20">
-  <div
-    class="flex flex-col items-center justify-evenly gap-[40px] max-w-[56rem] w-2/3">
-    <h2
-      class="2xl:text-home-title-2xl text-center text-home-secondary-title sm:text-home-title-md lg:text-home-title-lg xl:text-hero-title">
-      Content Credentials are rolling out across the internet
-    </h2>
+<div
+  class="flex flex-col items-center justify-center pb-10 pt-20 xl:w-[802px] 2xl:w-[910px] sm:px-[1.5rem] lg:max-w-[56rem] lg:w-2/3">
+  <h2
+    class="text-center text-home-secondary-title sm:text-home-title-md xl:text-hero-title 2xl:text-home-title-2xl">
+    Content Credentials are rolling out across the internet
+  </h2>
+  <div class="flex flex-col items-center justify-center pt-10">
     <p
-      class="2xl:text-hero-sub-desktop lg:text-hero-sub-tablet text-center text-hero-sub text-gray-900/60">
+      class="text-center text-hero-sub text-gray-900/60 lg:text-hero-sub-tablet 2xl:text-hero-sub-desktop">
       Expect to see the Content Credentials pin popping up in more and more
       places as it gets implemented by partners far and wide.
     </p>
