d593ccc8db7c245ac760720066ed5000e52610d3
Massy Mahamli <mmahamli@adobe.com>
2024-01-30 11:54:41 -0500

    tests added

--- src/routes/verify/components/AssetInfo/SmallAssetInfo.svelte
+++ src/routes/verify/components/AssetInfo/SmallAssetInfo.svelte
@@ -22,6 +22,7 @@
   export let highlighted = false;
   $: date = assetData.manifestData?.date;
   $: issuer = assetData.manifestData?.signatureInfo?.issuer;
+  $: console.log('assetData', assetData);
 </script>
 
 <AssetInfoThumbnailBase {assetData} {highlighted}>
